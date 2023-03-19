fbdb809e6344d5f06e9f6d2e48194e41ac2f98ee
Therealkingjimmy <robertsinjimmy2@gmail.com>
2023-03-19 10:50:29 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
+
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 
 {
 
-	char low, e, q;
+	int i;
 
-	e = 'e';
-	q = 'q';
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
 
-	for (low = 'a'; low <= 'z'; low++)
-	{
-	if (low != e && low != q)
-	putchar(low);
-	}
-	putchar('\n');
-	
 	return (0);
 
 }
